fb20ef10487a4c6950b4181d69ce4c34a614a4f3
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-06 23:56:00 +0530

    ok

---
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/dao/custom/PatcientDao.java
@@ -0,0 +1,8 @@
+package lk.ijse.orm.ormhibernatefinalcw.dao.custom;
+
+import lk.ijse.orm.ormhibernatefinalcw.dao.CrudDao;
+import lk.ijse.orm.ormhibernatefinalcw.entity.Patcient;
+
+public interface PatcientDao extends CrudDao<Patcient> {
+
+}
