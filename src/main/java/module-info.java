f9a6f1045259c31b825039d916febc8675111260
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-06 18:30:13 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.orm.ormhibernatefinalcw {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.orm.ormhibernatefinalcw to javafx.fxml;
+    exports lk.ijse.orm.ormhibernatefinalcw;
+}
