758433b1e9b0fa6f6375264f4bf0adeb187c09ae
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-06 23:49:12 +0530

    no bugs and all done

---
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/controller/ricieption/AddRicieptionController.java
@@ -0,0 +1,55 @@
+package lk.ijse.orm.ormhibernatefinalcw.controller.ricieption;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.orm.ormhibernatefinalcw.dto.RicieptionDto;
+import lk.ijse.orm.ormhibernatefinalcw.service.ServiceFactory;
+import lk.ijse.orm.ormhibernatefinalcw.service.custom.RicieptionService;
+import lk.ijse.orm.ormhibernatefinalcw.utils.WindowUtils;
+
+public class AddRicieptionController {
+
+    RicieptionService ricieptionService = (RicieptionService) ServiceFactory.getInstance().getService(Type.RICIEPTION);
+    @FXML
+    private TextField emailTxt;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private AnchorPane rolesvalidatePane;
+
+    @FXML
+    void gotoLandingPage(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("RolesView", rolesvalidatePane);
+    }
+
+    @FXML
+    void validateAdmin(ActionEvent event) throws Exception{
+
+        if (emailTxt.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Email Cannot be null !").show();
+        }
+
+        if (passwordTxt.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Password Cannot be null !").show();
+        }
+
+        Boolean resp = ricieptionService.addNewRicieption(
+                new RicieptionDto(
+                        emailTxt.getText(),
+                        passwordTxt.getText()
+                ));
+
+        if (resp) {
+            new Alert(Alert.AlertType.INFORMATION,"Ricieption Addedd Success !").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR,"Ricieption Added Failed !").show();
+        }
+
+    }
+}
