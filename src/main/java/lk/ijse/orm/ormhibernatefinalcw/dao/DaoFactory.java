8fd2f309f3910d002c8aedb505f02c7a81ce081c
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-06 23:53:15 +0530

    don

---
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/dao/DaoFactory.java
@@ -0,0 +1,39 @@
+package lk.ijse.orm.ormhibernatefinalcw.dao;
+
+import lk.ijse.orm.ormhibernatefinalcw.dao.custom.impl.*;
+
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+
+    private DaoFactory() {
+    }
+
+    public static DaoFactory getInstance() {
+        return daoFactory != null ? daoFactory : new DaoFactory();
+    }
+
+    public AdminDaoImpl getDao(Type type) {
+        switch (type) {
+            case ADMIN:
+                return new AdminDaoImpl();
+            case PATCIENT:
+                return new PatcientDaoImpl();
+            case RICIEPTION:
+                return new RicieptionDaoImpl();
+            case PROGRAMME:
+                return new ProgrammeDaoImpl();
+            case THEREPIST:
+                return new TherepistDaoImpl();
+            case THE_SESSION:
+                return new SessionDaoImpl();
+            case PAYMENT:
+                return new PatmentDaoImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum Type {
+        ADMIN, RICIEPTION, PATCIENT, PROGRAMME, THEREPIST,THE_SESSION,PAYMENT
+    }
+}
