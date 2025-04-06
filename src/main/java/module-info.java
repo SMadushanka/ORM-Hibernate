bcf9f2de094a91d191a3899a4c3a4c761a92be52
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-06 21:35:57 +0530

    req/opn/sx done

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,25 @@
 module lk.ijse.orm.ormhibernatefinalcw {
-    requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires com.jfoenix;
+    requires javafx.controls;
+    requires mysql.connector.java;
+
+
+
+    opens lk.ijse.orm.ormhibernatefinalcw.entity to org.hibernate.orm.core;
+    opens lk.ijse.orm.ormhibernatefinalcw.config to jakarta.persistence;
 
 
     opens lk.ijse.orm.ormhibernatefinalcw to javafx.fxml;
+    opens lk.ijse.orm.ormhibernatefinalcw.controller to javafx.fxml;
+    opens lk.ijse.orm.ormhibernatefinalcw.controller.ricieption to javafx.fxml;
+    opens lk.ijse.orm.ormhibernatefinalcw.dto to javafx.fxml;
+    opens lk.ijse.orm.ormhibernatefinalcw.dto.tm to javafx.fxml;
+
     exports lk.ijse.orm.ormhibernatefinalcw;
+    opens lk.ijse.orm.ormhibernatefinalcw.controller.admin to javafx.fxml;
 }
