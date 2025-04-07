9460bca65b70088be6317955fb1706d26cb4f6ca
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-07 19:33:09 +0530

    change code.

--- src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/PaymentService.java
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/PaymentService.java
@@ -1,7 +1,8 @@
 package lk.ijse.orm.ormhibernatefinalcw.service.custom;
 
 import lk.ijse.orm.ormhibernatefinalcw.dto.PaymentDto;
+import lk.ijse.orm.ormhibernatefinalcw.service.SuperService;
 
-public interface PaymentService {
+public interface PaymentService extends SuperService {
     boolean savePayment(PaymentDto paymentDto) throws Exception;
 }
