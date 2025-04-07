a17c6aceccbc6e737b70cfe84fbcdd513bf1b2bc
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-07 19:18:58 +0530

    imported success.

--- src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/AdminService.java
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/AdminService.java
@@ -1,9 +1,12 @@
 package lk.ijse.orm.ormhibernatefinalcw.service.custom;
 
 import lk.ijse.orm.ormhibernatefinalcw.dto.AdminDto;
+import lk.ijse.orm.ormhibernatefinalcw.service.SuperService;
 
 import java.sql.SQLException;
 
-public interface AdminService {
+public interface AdminService extends SuperService {
+
     Boolean addNewAdmin(AdminDto adminDto) throws Exception;
+
 }
