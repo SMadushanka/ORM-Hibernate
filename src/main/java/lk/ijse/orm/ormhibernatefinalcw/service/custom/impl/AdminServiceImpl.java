35d3e6fe8107d0547f599055e17e68a62eeac956
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-07 19:19:56 +0530

    imported success. and no error.

--- src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/impl/AdminServiceImpl.java
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/impl/AdminServiceImpl.java
@@ -1,16 +1,18 @@
 package lk.ijse.orm.ormhibernatefinalcw.service.custom.impl;
 
 import lk.ijse.orm.ormhibernatefinalcw.dao.DaoFactory;
+import lk.ijse.orm.ormhibernatefinalcw.dao.DaoFactory.Type;
 import lk.ijse.orm.ormhibernatefinalcw.dao.custom.AdminDao;
 import lk.ijse.orm.ormhibernatefinalcw.dto.AdminDto;
 import lk.ijse.orm.ormhibernatefinalcw.entity.Admin;
 import lk.ijse.orm.ormhibernatefinalcw.service.custom.AdminService;
 
-public class AdminServiceImpl implements AdminService {
-    AdminDao adminDao = (AdminDao) DaoFactory.getInstance().getDao(DaoFactory.Type.ADMIN);
+public class AdminServiceImpl implements AdminService{
+
+    AdminDao adminDao = (AdminDao) DaoFactory.getInstance().getDao(Type.ADMIN);
 
     @Override
-    public Boolean addNewAdmin(AdminDto adminDto) throws Exception {
+    public Boolean addNewAdmin(AdminDto adminDto) throws Exception{
         return adminDao.save(new Admin(adminDto.getEmail(),adminDto.getPassword()));
     }
 
