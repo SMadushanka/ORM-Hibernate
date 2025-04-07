56f98f416edbdaf9003a6bb1c1d15630fd938b96
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-07 19:33:49 +0530

    change code.

--- src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/RicieptionService.java
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/service/custom/RicieptionService.java
@@ -1,10 +1,12 @@
 package lk.ijse.orm.ormhibernatefinalcw.service.custom;
 
 import lk.ijse.orm.ormhibernatefinalcw.dto.RicieptionDto;
+import lk.ijse.orm.ormhibernatefinalcw.service.SuperService;
 
 import java.sql.SQLException;
 
-public interface RicieptionService {
+public interface RicieptionService extends SuperService {
 
     Boolean addNewRicieption(RicieptionDto ricieptionDto) throws Exception;
+
 }
