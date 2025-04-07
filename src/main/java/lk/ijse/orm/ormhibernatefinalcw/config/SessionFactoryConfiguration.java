62ccbde2c5b245264bd88a88141386c9e6804326
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-07 19:40:19 +0530

    change all code and fix all error.

--- src/main/java/lk/ijse/orm/ormhibernatefinalcw/config/SessionFactoryConfiguration.java
+++ src/main/java/lk/ijse/orm/ormhibernatefinalcw/config/SessionFactoryConfiguration.java
@@ -1,32 +1,34 @@
 package lk.ijse.orm.ormhibernatefinalcw.config;
 
-import com.mysql.cj.xdevapi.SessionFactory;
 import lk.ijse.orm.ormhibernatefinalcw.entity.*;
 import org.hibernate.Session;
-
-import java.lang.module.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class SessionFactoryConfiguration {
     private static SessionFactoryConfiguration sessionFactoryConfiguration;
     private final SessionFactory sessionFactory;
 
     private SessionFactoryConfiguration() {
-        Configuration configuration = new Configuration();
+        org.hibernate.cfg.Configuration configuration = new Configuration();
         configuration.configure();
         configuration.addAnnotatedClass(Admin.class)
                 .addAnnotatedClass(Riciepion.class)
+                .addAnnotatedClass(Patcient.class)
                 .addAnnotatedClass(Programme.class)
                 .addAnnotatedClass(Therepist.class)
                 .addAnnotatedClass(ThereSession.class)
                 .addAnnotatedClass(Payment.class);
         sessionFactory = configuration.buildSessionFactory();
     }
+
     public static SessionFactoryConfiguration getInstance() {
         if (sessionFactoryConfiguration == null) {
             sessionFactoryConfiguration = new SessionFactoryConfiguration();
         }
         return sessionFactoryConfiguration;
     }
+
     public Session getSession() {
         return sessionFactory.openSession();
     }
