87a2e6449d185cf6bf027ba800c1d3980dca938c
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-24 14:38:57 +0530

    change code.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,16 +9,14 @@ module lk.ijse.orm.ormhibernatefinalcw {
     requires mysql.connector.java;
     requires java.desktop;
 
-
     opens lk.ijse.orm.ormhibernatefinalcw.entity to org.hibernate.orm.core;
     opens lk.ijse.orm.ormhibernatefinalcw.config to jakarta.persistence;
-
 
     opens lk.ijse.orm.ormhibernatefinalcw to javafx.fxml;
     opens lk.ijse.orm.ormhibernatefinalcw.controller to javafx.fxml;
     opens lk.ijse.orm.ormhibernatefinalcw.controller.ricieption to javafx.fxml;
     opens lk.ijse.orm.ormhibernatefinalcw.dto to javafx.fxml;
-    opens lk.ijse.orm.ormhibernatefinalcw.dto.tm to javafx.fxml;
+    opens lk.ijse.orm.ormhibernatefinalcw.dto.tm to javafx.fxml, javafx.base;
 
     exports lk.ijse.orm.ormhibernatefinalcw;
     opens lk.ijse.orm.ormhibernatefinalcw.controller.admin to javafx.fxml;
